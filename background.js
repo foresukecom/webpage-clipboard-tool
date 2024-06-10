76866d6d28c73169588c2e05a6977e9c6b442baf
foresuke <foresukecom@gmail.com>
2024-06-10 22:28:03 +0900

    ✨ コピーするフォーマットを追加

--- background.js
+++ background.js
@@ -1,25 +1,68 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: "copyPageInfo",
-    title: "URLコピー(format: markdown)",
+    id: "parent",
+    title: "ページ情報コピー",
+    contexts: ["page"]
+  });
+
+  chrome.contextMenus.create({
+    id: "copyTitle",
+    parentId: "parent",
+    title: "タイトル",
+    contexts: ["page"]
+  });
+
+  chrome.contextMenus.create({
+    id: "copyURL",
+    parentId: "parent",
+    title: "URL",
+    contexts: ["page"]
+  });
+
+  chrome.contextMenus.create({
+    id: "copyMarkdown",
+    parentId: "parent",
+    title: "Markdown link",
+    contexts: ["page"]
+  });
+
+  chrome.contextMenus.create({
+    id: "copyTitleAndURL",
+    parentId: "parent",
+    title: "タイトル&URL",
     contexts: ["page"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "copyPageInfo") {
-    const markdownLink = `[${tab.title}](${tab.url})`;
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['content.js']
-    }, () => {
-      chrome.tabs.sendMessage(tab.id, {action: 'copy', text: markdownLink}, (response) => {
-        if (response && response.status === 'success') {
-          console.log('Copied to clipboard');
-        } else {
-          console.error('Failed to copy');
-        }
-      });
-    });
+  if (info.menuItemId === "copyTitle") {
+    const text = tab.title;
+    copyTextToClipboard(tab.id, text);
+  } else if (info.menuItemId === "copyURL") {
+    const text = tab.url;
+    copyTextToClipboard(tab.id, text);
+  } else if (info.menuItemId === "copyMarkdown") {
+    const text = `[${tab.title}](${tab.url})`;
+    copyTextToClipboard(tab.id, text);
+  } else if (info.menuItemId === "copyTitleAndURL") {
+    const text = `${tab.title}\n${tab.url}`;
+    copyTextToClipboard(tab.id, text);
   }
 });
+
+function copyTextToClipboard(tabId, text) {
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: (text) => {
+      const input = document.createElement('textarea');
+      input.style.position = 'fixed';
+      input.style.opacity = '0';
+      input.value = text;
+      document.body.appendChild(input);
+      input.select();
+      document.execCommand('Copy');
+      document.body.removeChild(input);
+    },
+    args: [text]
+  });
+}
